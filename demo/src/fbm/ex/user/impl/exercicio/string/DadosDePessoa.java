a22065a5b52452c2c2485d1b3fa5a10eb805dec7
felipeDevBino <felipereisbino@gmail.com>
2023-10-21 18:33:55 -0300

    Enviando pequenas alterações
    Alterações referentes a ArrayList e entendimento da informação

--- demo/src/fbm/ex/user/impl/exercicio/string/DadosDePessoa.java
+++ demo/src/fbm/ex/user/impl/exercicio/string/DadosDePessoa.java
@@ -14,16 +14,16 @@ public class DadosDePessoa extends ExGeneric {
 
 	ArrayList<String> pessoas = new ArrayList<String>();
 	ArrayList<String> dados = new ArrayList<String>();
-	ArrayList<String> dadosEPessoas = new ArrayList<>();
+	ArrayList<String> dadosEPessoas = new ArrayList<String>();
 	
 	@Override
 	public void iniciarExercicio() throws BusinessException {
 
-		pessoas.add("Junior"); dados.add("Email: juniorsouza@gmail.com / Telefone: 998801342");
-		pessoas.add("Alexandre"); dados.add("Email: alexandremota@gmail.com / Telefone: 997821342");
-		pessoas.add("Ana"); dados.add("Email: anajulia@outlook.com / Telefone: 998201434");
-		pessoas.add("Julia"); dados.add("Email: juliamoreira@outlook.com / Telefone: 991516542");
-		pessoas.add("Maria"); dados.add("Email: mariaelizabeth@gmail.com / Telefone: 995402263");
+		pessoas.add("Junior "); dados.add("Email: juniorsouza@gmail.com / Telefone: 998801342");
+		pessoas.add("Alexandre "); dados.add("Email: alexandremota@gmail.com / Telefone: 997821342");
+		pessoas.add("Ana "); dados.add("Email: anajulia@outlook.com / Telefone: 998201434");
+		pessoas.add("Julia "); dados.add("Email: juliamoreira@outlook.com / Telefone: 991516542");
+		pessoas.add("Maria "); dados.add("Email: mariaelizabeth@gmail.com / Telefone: 995402263");
 		
 	}
 
@@ -31,7 +31,7 @@ public class DadosDePessoa extends ExGeneric {
 	public void processarExercicio() throws BusinessException {
 
 		for(int ad = 0; ad < pessoas.size(); ad++) {
-			dadosEPessoas.add("Nome: " + pessoas.get(ad) + ", possui os dados de: " + dados.get(ad));
+			dadosEPessoas.add("Nome: " + pessoas.get(ad) + "/ possui os dados de: " + dados.get(ad));
 		}
 	
 	} 
