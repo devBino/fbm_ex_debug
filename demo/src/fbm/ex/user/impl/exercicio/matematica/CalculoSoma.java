ca1cec7842a54c718329f35fec28c0ad8daf6c41
Paulo Barbosa <pbarbosa4410@gmail.com>
2023-10-26 16:05:13 -0300

    Update CalculoSoma.java

--- demo/src/fbm/ex/user/impl/exercicio/matematica/CalculoSoma.java
+++ demo/src/fbm/ex/user/impl/exercicio/matematica/CalculoSoma.java
@@ -2,8 +2,27 @@ package fbm.ex.user.impl.exercicio.matematica;
 
 public class CalculoSoma {
 
+	public void iniciarExercicio() throws BusinessException {
+
+		Scanner sc = new Scanner(System.in);
+
+		int A, B, soma;
+
+		A = sc.nextInt();
+		B = sc.nextInt();
+
+		soma = A + B;
+
+		System.out.println("SOMA = " + soma);
+
+		sc.close();
+	}
 	
-	
+	/**
+	 * Processamos o exercício, nessa etapa processamos os parametros 
+	 * com alguma lógica que deve produzir novas variáveis que poderão ser 
+	 * usadas na saída do exercício
+	 */
 	
 	
 	
