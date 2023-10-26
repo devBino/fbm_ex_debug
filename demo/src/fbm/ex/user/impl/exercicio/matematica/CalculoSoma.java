32b05b0dfd2ba47223c6a66d2fc48c509a6e629d
Paulo Barbosa <pbarbosa4410@gmail.com>
2023-10-26 16:12:44 -0300

    Update CalculoSoma.java

--- demo/src/fbm/ex/user/impl/exercicio/matematica/CalculoSoma.java
+++ demo/src/fbm/ex/user/impl/exercicio/matematica/CalculoSoma.java
@@ -18,19 +18,23 @@ public class CalculoSoma {
 		sc.close();
 	}
 	
-	/**
-	 * Processamos o exercício, nessa etapa processamos os parametros 
-	 * com alguma lógica que deve produzir novas variáveis que poderão ser 
-	 * usadas na saída do exercício
-	 */
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+}
+
+public class Main {
+
+public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double R, A, pi = 3.14159;
+	    
+	    R = sc.nextDouble();
+
+	    A = pi * R * R;
+
+	    System.out.printf("A=%.4f%n", A);
+
+		sc.close();
+	}
 }
